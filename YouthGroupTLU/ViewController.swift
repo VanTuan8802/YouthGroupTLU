aa949b22b2aa456eeb7ab59077ee6e11deff94c2
VanTuan8802 <tuannv@vtn-global.com>
2024-02-28 16:02:24 +0700

    init project

---
+++ YouthGroupTLU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouthGroupTLU
+//
+//  Created by Moon Dev on 28/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
